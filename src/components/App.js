09f5872672df7b22733f261a40a11d2129f513a7
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-09-11 11:30:51 -0700

    Creating context objects

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import UserCreate from './UserCreate';
 
 class App extends React.Component {
 
@@ -15,7 +16,7 @@ class App extends React.Component {
                     <i onClick={() => this.onLanguageChange('english')} className="flag us" />
                     <i onClick={() => this.onLanguageChange('dutch')} className="flag nl" />
                 </div>
-                {this.state.language}
+                <UserCreate />
             </div>
         );
     }
---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+
+class Button extends React.Component {
+
+    //MUST be called contextType
+    static contextType = LanguageContext;
+
+    render() {
+        console.log(this.context);
+        return <button className="ui button primary">Submit</button>;
+    }
+}
+
+export default Button;
