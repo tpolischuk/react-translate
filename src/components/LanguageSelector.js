d77054a36e70ed7c65e85e7b03d0854ab6ef78d2
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-09-12 15:59:42 -0700

    Rendering the language store

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -5,9 +5,8 @@ class LanguageSelector extends React.Component {
         return (
             <div>
                 Select a language:
-            <i onClick={() => this.props.onLanguageChange('english')} className="flag us" />
+                <i onClick={() => this.props.onLanguageChange('english')} className="flag us" />
                 <i onClick={() => this.props.onLanguageChange('dutch')} className="flag nl" />
-
             </div>);
 
     }
--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+const Context = React.createContext('english');
 
+export class LanguageStore extends React.Component {
+    state = { langauge: 'english' };
 
-const context = React.createContext('english');
+    onLanguageChange = (language) => {
+        this.setState({ language });
+    };
 
-console.log(context);
+    render() {
+        return (
+            <Context.Provider value={{ ...this.state, onLanguageChange: this.onLanguageChange }}>
+                {this.props.children}
+            </Context.Provider>
+        );
+    }
+}
 
-export default context;
+export default Context;
